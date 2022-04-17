97d9157d0c4547ae973fd5c1aec6a764dbe448e0
Aryal007 <bibekaryal7@gmail.com>
2022-04-17 19:23:07 +0000

    Find lr

--- Classifier/sc.py
+++ Classifier/sc.py
@@ -85,6 +85,49 @@ class LTNClassifier(pl.LightningModule):
         loader = getLoader(**self.val_loader_args)
         return loader
 
+    def find_lr(self, init_value, final_value):
+        train_loader = getLoader(**self.train_loader_args)
+        number_in_epoch = len(train_loader) - 1
+        update_step = (final_value / init_value) ** (1 / number_in_epoch)
+        lr = init_value
+        optimizer = self.configure_optimizers()
+        optimizer.param_groups[0]["lr"] = lr
+        best_loss = 0.0
+        batch_num = 0
+        losses = []
+        log_lrs = []
+        iterator = tqdm(train_loader, desc="Current lr=XX.XX Steps=XX Loss=XX.XX Best lr=XX.XX ")
+        for i, data in enumerate(iterator):
+            batch_num += 1
+            inputs, labels = data[f"{self.streamtype}_1"], data[f"{self.streamtype}_label"]
+            optimizer.zero_grad()
+            prob, _ = self.model(inputs, softmax=True)
+            loss = torch.nn.CrossEntropyLoss()(prob, labels)
+            # Crash out if loss explodes
+            if batch_num > 1 and loss > 4 * best_loss:
+                return log_lrs[10:-5], losses[10:-5]
+            # Record the best loss
+            if loss < best_loss or batch_num == 1:
+                best_loss = loss
+                best_lr = lr
+            # Do the backward pass and optimize
+            loss.backward()
+            self.optimizer.step()
+            iterator.set_description("Current lr=%5.9f Steps=%d Loss=%5.3f Best lr=%5.9f " %(lr, i, loss, best_lr))
+            # Store the values
+            losses.append(loss.detach())
+            log_lrs.append(math.log10(lr))
+            # Update the lr for the next step and store
+            lr = lr * update_step
+            self.optimizer.param_groups[0]["lr"] = lr
+        logs, losses = log_lrs[10:-5], losses[10:-5]
+
+        plt.plot(logs, losses)
+        plt.xlabel("learning rate (log scale)")
+        plt.ylabel("loss")
+        plt.savefig("Optimal lr curve.png")
+        print("plot saved")
+
 if __name__ == '__main__':
     checkpoint_callback = ModelCheckpoint(
     monitor="val_loss",
